8a90b1c47e64f81428b5ffa2d2cda6a90161aef8
Chad Torkkola <ctorkkola#gmail.com>
2020-01-14 20:19:09 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Strings___collections_and_hello_s___TheDevelopingDeveloper.net
+{
+    class Program
+    {
+        //create a string array
+        //add some names to the array
+        //use a for loop to iterate through the array
+        //write out the console hello + each name
+        //write out the console hello + each name
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
